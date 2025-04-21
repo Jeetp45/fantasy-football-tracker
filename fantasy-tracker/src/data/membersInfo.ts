59def243b9f17e4d64536357a3b11d0628ad870b
Jeetp45 <141786979+Jeetp45@users.noreply.github.com>
2025-04-21 13:26:07 -0400

    changed image imporst

--- fantasy-tracker/src/data/membersInfo.ts
+++ fantasy-tracker/src/data/membersInfo.ts
@@ -1,64 +1,72 @@
 import { Member } from '@/types/member';
+import packers from '@/assets/packers.png';
+import panthers from '@/assets/panthers.png';
+import bears from '@/assets/bears.png';
+import patriots from '@/assets/patriots.jpg';
+import giants from '@/assets/giants.png';
+import cowboys from '@/assets/cowboys.png';
+import falcons from '@/assets/falcons.png';
+import lions from '@/assets/lions.png';
 
 export const members: Member[] = [
   {
     name: 'Anthony Ramirez',
     team: 'Cooper sucks',
-    avatar: '/src/assets/packers.png',
+    avatar: packers,
     wins: 0,
   },
   {
     name: 'Bishop Richardson',
     team: 'columbus flyz',
-    avatar: '/src/assets/panthers.png',
+    avatar: panthers,
     wins: 0,
   },
   {
     name: 'Campbell Wildrick',
     team: 'LoveMeCujo',
-    avatar: '/src/assets/panthers.png',
+    avatar: panthers,
     wins: 1,
   },
   {
     name: 'Clay Killoren',
     team: 'ckilla',
-    avatar: '/src/assets/bears.png',
+    avatar: bears,
     wins: 1,
   },
   {
     name: 'Cooper Hannah',
     team: 'The Fitzgeralds',
-    avatar: '/src/assets/panthers.png',
+    avatar: panthers,
     wins: 2,
   },
   {
     name: 'Hank Barron',
     team: 'Red Barons',
-    avatar: '/src/assets/patriots.jpg',
+    avatar: patriots,
     wins: 3,
   },
   {
     name: 'Jeet Patel',
     team: 'Patel Brothers',
-    avatar: '/src/assets/giants.png',
+    avatar: giants,
     wins: 1,
   },
   {
     name: 'Noah Beason',
     team: 'Norts',
-    avatar: '/src/assets/cowboys.png',
+    avatar: cowboys,
     wins: 1,
   },
   {
     name: 'Rett Foust',
     team: 'Atlanta ReJulios',
-    avatar: '/src/assets/falcons.png',
+    avatar: falcons,
     wins: 1,
   },
   {
     name: 'Robert Lyon',
     team: 'Detroit Lyons',
-    avatar: '/src/assets/lions.png',
+    avatar: lions,
     wins: 0,
   },
 ];
